d13590dd4c4c88bb3882ec5782140fd6783c5aaf
paranoid <1817214890@qq.com>
2026-09-24 00:57:51 +0800

    test1: 使用更安全的fgets，但是fgets会读进换行

--- test1.c
+++ test1.c
@@ -4,8 +4,13 @@ int main() {
     char name[100];//字符数组，100个字节足够存30多个汉字
 
     printf("请输入你的中文名：\n");
-    scanf(" %s", name); 
+    // scanf(" %s", name); 
     // 数组名 name 本身代表地址，scanf 里无需添加取地址符 &
+
+    // 更安全的写法：使用 fgets 
+    // （用 fgets() 代替 scanf("%s")，因为 scanf 遇到空格就会停止读取，而 fgets 可以读取一整行（包括空格））
+    fgets(name, sizeof(name), stdin);
+    // 但是 fgets 会读进换行，只能读完后用 name[strcspn(name, "\n")] = '\0'; 把换行符替换成字符串结束符删掉
 
     printf("你的中文名是：%s\n", name);
 
