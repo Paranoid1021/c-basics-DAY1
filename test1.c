fef4aa8f1ab0e207d9906d278e2e39e2ea705c7a
paranoid <1817214890@qq.com>
2026-09-24 00:41:41 +0800

    test1: 增加输入提示语，注意scanf吃回车的问题

--- test1.c
+++ test1.c
@@ -3,8 +3,12 @@
 int main() {
     char a;
     int b = 10;
+
     printf("请输入一个字母\n");
     scanf(" %c", &a); // 加空格跳过空白字符
-    printf("%c\n%d\n", a, b);
+
+    printf("你输入的字符是：%c\n", a);
+    printf("变量 b 的值是：%d\n", b);
+    
     return 0;
 }
