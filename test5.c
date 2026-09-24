15ac753119362df391675024f63bd070e034291d
paranoid <1817214890@qq.com>
2026-09-24 17:49:29 +0800

    摄氏转华氏温度转换（补充了可能产生的错误的注释）

--- test5.c
+++ test5.c
@@ -8,6 +8,7 @@ int main() {
     
     float fahrenheit = (celsius * 9 / 5) + 32;
     printf("摄氏温度 %.2f 转换为华氏温度为：%.2f\n", celsius, fahrenheit);
-
+    // 如果在 写成 (9 / 5) * celsius + 32，那么 9 / 5 因为是整数除法，会变成 1，结果就会错成 1 * 36.5 + 32 = 68.5
+    // 所以要写成 (celsius * 9 / 5) + 32，先将 celsius 转换为浮点数，再进行运算，结果才会正确
     return 0;
 }
